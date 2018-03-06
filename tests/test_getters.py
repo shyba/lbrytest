c4f5fe5daa53948652cf48b3384d9c0e3c199fbb
Lex Berezhny <lex@damoti.com>
2018-03-05 19:51:57 -0500

    finally reliable tests

--- tests/test_getters.py
+++ tests/test_getters.py
@@ -1,23 +1,18 @@
 from lbrytest.case import IntegrationTestCase
 from twisted.internet import defer
 
-import logging
-logging.getLogger('lbrynet').setLevel(logging.DEBUG)
-logging.getLogger('lbryum').setLevel(logging.DEBUG)
-
 
 class BalanceTest(IntegrationTestCase):
 
-    VERBOSE = True
-    USE_FIXTURE = True
-
     @defer.inlineCallbacks
     def test_balance(self):
-        address = yield self.lbry.wallet.get_least_used_address()
-        sendtxid = yield self.lbrycrd.sendtoaddress(address, 1)
+        yield self.wallet.update_balance()
+        self.assertEqual(self.wallet.wallet_balance, 0)
+
+        address = yield self.wallet.get_least_used_address()
+        sendtxid = yield self.lbrycrd.sendtoaddress(address, 2.5)
         yield self.lbrycrd.generate(1)
         yield self.lbry.wallet.wait_for_tx_in_wallet(sendtxid)
-        print(self.lbry.wallet.wallet_balance)
+
         yield self.lbry.wallet.update_balance()
-        print(self.lbry.wallet.wallet_balance)
-        self.assertEqual(self.lbry.wallet.wallet_balance, 1)
+        self.assertEqual(self.lbry.wallet.wallet_balance, 2.5)

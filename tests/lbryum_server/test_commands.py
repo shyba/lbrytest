20011aef3f6de78b0121ef254359746792dd7db3
Victor Shyba <victor1984@riseup.net>
2018-03-10 15:33:58 -0300

    add claim dict data for lbryum server tests

--- tests/lbryum_server/test_commands.py
+++ tests/lbryum_server/test_commands.py
@@ -14,6 +14,26 @@ Integration tests for lbryum-server using stratum protocol
 TODO: reorganize tests after it becomes clear which fixtures to use and how to better group in scenarios
 """
 
+test_metadata = {
+    'license': 'NASA',
+    'version': '_0_1_0',
+    'description': 'test',
+    'language': 'en',
+    'author': 'test',
+    'title': 'test',
+    'nsfw': False,
+    'thumbnail': 'test'
+}
+
+test_claim_dict = {
+    'version': '_0_0_1',
+    'claimType': 'streamType',
+    'stream': {'metadata': test_metadata, 'version': '_0_0_1', 'source':
+        {'source': '8655f713819344980a9a0d67b198344e2c462c90f813e86f'
+                   '0c63789ab0868031f25c54d0bb31af6658e997e2041806eb',
+         'sourceType': 'lbry_sd_hash', 'contentType': 'video/mp4', 'version': '_0_0_1'},
+               }}
+
 
 class CommandsTestCase(IntegrationTestCase):
 
